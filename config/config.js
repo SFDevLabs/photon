7f08a2c7cb50e0137a4fb5fca7b3757ec78ecda5
jeffj <jenkinsjeffrey@gmail.com>
2015-01-20 22:07:48 -0800

    stuff

--- config/config.js
+++ config/config.js
@@ -33,4 +33,4 @@ module.exports = {
   test: extend(test, defaults),
   production: extend(production, defaults)
 }[process.env.NODE_ENV || 'development'];
-console.log(module.exports,'module.exports')
+console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
