f7912c3c3da26d611535d072ee2e73398e3a95b3
jeffj <jenkinsjeffrey@gmail.com>
2015-02-07 18:02:35 -0800

    signup fixes.

--- test/helper.js
+++ test/helper.js
@@ -5,7 +5,7 @@
 
 var mongoose = require('mongoose')
   , async = require('async')
-  , Article = mongoose.model('Article')
+  , Article = mongoose.model('Album')
   , User = mongoose.model('User')
 
 /**
