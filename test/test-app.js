0d33c826d0e02378b7489c08a21a4f4ab139231e
jeffj <jenkinsjeffrey@gmail.com>
2015-02-08 01:29:07 -0800

    adding front end test block.

---
+++ test/test-app.js
@@ -0,0 +1,19 @@
+
+/**
+ * Module dependencies.
+ */
+
+
+/**
+ * App tests
+ */
+
+describe('App', function () {
+   it('Totally Testing', function (done) {
+    if (-1 === [1,2,3,4].indexOf(5)){
+      done()
+    }else{
+      throw "Test Failed";
+    }
+   })  
+})
